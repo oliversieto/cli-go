2765b7c99b82bff1931cd1386587886b21750809
Renato Oliveira <oliversieto@gmail.com>
2022-02-27 18:28:10 -0400

    feat: add command host
    host command get IPS through internal package net.

--- app/app.go
+++ app/app.go
@@ -1,11 +1,41 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // Gerar vai retornar a aplicação de linha de comando pronta para ser executada.
 func Generate() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Aplicação de linha de comando"
 	app.Usage = "Busca Ips e Nomes de servidores na internet"
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Busca IPS de endereços na internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "devbook.com.br",
+				},
+			},
+			Action: getIps,
+		},
+	}
 	return app
+}
+
+func getIps(c *cli.Context) {
+	host := c.String("host")
+	ips, error := net.LookupIP(host)
+	if error != nil {
+		log.Fatal(error)
+	}
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
