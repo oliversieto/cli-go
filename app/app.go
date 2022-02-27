c6152ea7964552cbcd6976b5cee81500b9918494
Renato Oliveira <oliversieto@gmail.com>
2022-02-27 18:35:47 -0400

    feat: add command servidores
    servidores command get hosts through internal package net

--- app/app.go
+++ app/app.go
@@ -13,17 +13,24 @@ func Generate() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Aplicação de linha de comando"
 	app.Usage = "Busca Ips e Nomes de servidores na internet"
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "devbook.com.br",
+		},
+	}
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Busca IPS de endereços na internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "devbook.com.br",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Busca IPS de endereços na internet",
+			Flags:  flags,
 			Action: getIps,
+		},
+		{
+			Name:   "servidores",
+			Usage:  "Busca o nome dos servidores na internet",
+			Flags:  flags,
+			Action: getServers,
 		},
 	}
 	return app
@@ -37,5 +44,16 @@ func getIps(c *cli.Context) {
 	}
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func getServers(c *cli.Context) {
+	host := c.String("host")
+	servers, error := net.LookupNS(host)
+	if error != nil {
+		log.Fatal(error)
+	}
+	for _, server := range servers {
+		fmt.Println(server.Host)
 	}
 }
